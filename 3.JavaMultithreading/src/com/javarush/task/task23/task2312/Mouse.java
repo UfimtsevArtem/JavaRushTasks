515b65af8886c40251598e2ef3287b0b46a80520
UfimtsevArtem <ufimtsauartem@gmail.com>
2017-04-13 16:34:26 +0300

    new

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -1,7 +1,19 @@
 package com.javarush.task.task23.task2312;
 
-/**
- * Created by User on 12.04.2017.
- */
 public class Mouse {
+    private int x;
+    private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task23.task2312;
 
-/**
- * Created by User on 12.04.2017.
- */
-public class SnakeDirection {
+
+public enum SnakeDirection {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
 }
